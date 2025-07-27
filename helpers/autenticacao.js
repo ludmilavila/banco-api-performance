f8bb6d35595eece1e2535094e6ad9cf29c525ac6
Ludmila Ávila <Ludmila Avila>
2025-07-27 16:08:34 -0300

    Adicionando função de helper para obtenção de token e criando o teste de transferência

---
+++ helpers/autenticacao.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'));
+
+export function obterToken () {
+    const url = 'http://localhost:3000/login';
+        const payload = JSON.stringify(postLogin);
+        const params = {
+            headers: {
+            'Content-Type': 'application/json',
+            },
+        };
+    
+    const resposta = http.post(url, payload, params);
+
+    return resposta.json('token');
+}
